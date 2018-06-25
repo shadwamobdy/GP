8f7ace497b614ab312949c8ceb1b6a420c6c898c
Unknown <shadwa.mobdy@gmail.com>
2018-06-25 21:33:15 +0200

    Update Apps
    Add Interior design application and modify the UI of fashion app.

--- InteriorDesign/app/controllers/main_controller.rb
+++ InteriorDesign/app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
-
+  
 	require 'uri'
 	require 'base64'
 	require 'net/http'
 	require 'net/https'
-
+	
 	@@IP = nil
 
 	def index
@@ -12,20 +12,20 @@ class MainController < ApplicationController
 	end
 
 	def init
-		@description = params[:anything][:description]
-		@img = params[:anything][:pose]
-		@name = "download1.png"
-		File.open(Rails.root.join('app', 'assets', 'images', @name), 'wb') do |file|
-		    file.write(@img.read)
-		end
+	  	@description = params[:anything][:description]
+		# @img = params[:anything][:pose]
+		# @name = "download1.png"
+		# File.open(Rails.root.join('app', 'assets', 'images', @name), 'wb') do |file|
+		#     file.write(@img.read)
+		# end
 		@description.gsub(" ", "+")
 		uri = "http://#{@@IP}:8080/process?desc=#{@description}"
 		url = URI.encode(uri)
 		url = URI.parse(url)
-		req = Net::HTTP::Post.new(url.to_s)
-		file = open("app/assets/images/download1.png")
-		req.body = file.read
-		req["Content-Type"] = "image/png"
+		req = Net::HTTP::Get.new(url.to_s)
+		# file = open("app/assets/images/download1.png")
+		# req.body = file.read
+		# req["Content-Type"] = "image/png"
 		res = Net::HTTP.start(url.host, url.port, read_timeout: 120) {|http|
 			response = http.request(req)
 			File.open(Rails.root.join('app', 'assets', 'images', 'output', 'output.png'), 'wb') do |file|
@@ -33,5 +33,4 @@ class MainController < ApplicationController
 			end
 		}
 	end
-
 end
